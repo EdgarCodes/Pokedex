860168236e270313584b374f9d0f88d64da5e945
Edgar Gonzalez <Edggar@Gmail.com>
2020-09-07 22:07:21 -0400

    Updates

---
+++ precache-manifest.70e4cc323e871ea2998b34fe1cdf1c39.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f008bb82e2d9233a8662866feaf04416",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4316ef4e493b68a0b36b",
+    "url": "/static/css/main.3736519d.chunk.css"
+  },
+  {
+    "revision": "ea3c8d88832276a04f1f",
+    "url": "/static/js/2.a20945a0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a20945a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4316ef4e493b68a0b36b",
+    "url": "/static/js/main.d0ab4e79.chunk.js"
+  },
+  {
+    "revision": "d5adf4f202db5e1aa213",
+    "url": "/static/js/runtime-main.54a2cc93.js"
+  },
+  {
+    "revision": "9796cb994e62c6c829bcafee0f10580a",
+    "url": "/static/media/PokemonTypes_01.9796cb99.png"
+  },
+  {
+    "revision": "c5a34105ac935e63ecaae16df470bd6c",
+    "url": "/static/media/PokemonTypes_02.c5a34105.png"
+  },
+  {
+    "revision": "aea01696c4cf769231d130045859902e",
+    "url": "/static/media/PokemonTypes_03.aea01696.png"
+  },
+  {
+    "revision": "fd99318194f34d25dbadad519cdacb7f",
+    "url": "/static/media/PokemonTypes_04.fd993181.png"
+  },
+  {
+    "revision": "a057848ed27269cea27fefbaa40d3313",
+    "url": "/static/media/PokemonTypes_05.a057848e.png"
+  },
+  {
+    "revision": "b9bd5557279ed928e868e89de12c6de4",
+    "url": "/static/media/PokemonTypes_06.b9bd5557.png"
+  },
+  {
+    "revision": "69f005418b464508633ac03470545f85",
+    "url": "/static/media/PokemonTypes_07.69f00541.png"
+  },
+  {
+    "revision": "f5f0d58c0b201ae70d01eb791f191c45",
+    "url": "/static/media/PokemonTypes_08.f5f0d58c.png"
+  },
+  {
+    "revision": "90a34e0754a243f3514eee134e3b443e",
+    "url": "/static/media/PokemonTypes_09.90a34e07.png"
+  },
+  {
+    "revision": "5d3ffdcff23ebdb1169f703ad18f5a56",
+    "url": "/static/media/PokemonTypes_10.5d3ffdcf.png"
+  },
+  {
+    "revision": "d59e3ea53df9956d206aa767e1b3d2a7",
+    "url": "/static/media/PokemonTypes_11.d59e3ea5.png"
+  },
+  {
+    "revision": "2af032e1fa86fb8563791cabb42ac372",
+    "url": "/static/media/PokemonTypes_12.2af032e1.png"
+  },
+  {
+    "revision": "6003c4454b1ae03d39a2a3276a0b302b",
+    "url": "/static/media/PokemonTypes_13.6003c445.png"
+  },
+  {
+    "revision": "ec07f7fddf8d461c09fbca404224614d",
+    "url": "/static/media/PokemonTypes_14.ec07f7fd.png"
+  },
+  {
+    "revision": "992f39204e402088092776001fcbe794",
+    "url": "/static/media/PokemonTypes_15.992f3920.png"
+  },
+  {
+    "revision": "6270794847976fb83df510fc8b79c342",
+    "url": "/static/media/PokemonTypes_16.62707948.png"
+  },
+  {
+    "revision": "35711eaa14f54e01477581b386ad9dae",
+    "url": "/static/media/PokemonTypes_17.35711eaa.png"
+  },
+  {
+    "revision": "a07ccb67796d692b1d0b11dacc1c0a9c",
+    "url": "/static/media/PokemonTypes_18.a07ccb67.png"
+  },
+  {
+    "revision": "174f6be2c5e4791df51924df01dd5e7c",
+    "url": "/static/media/pokeball.174f6be2.png"
+  }
+]);
